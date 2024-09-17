42cb1e308272cfb27ee2d6a4d1eeb780aaad1746
XenusVale <95154384+XenusVale@users.noreply.github.com>
2024-09-17 11:41:39 -0600

    Github Migration
    Migrated the code to github.

---
+++ Decompression Project/Decompression Project/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Decompression_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = args[0];
+            if (File.Exists(filePath))
+            {
+                var file = new FileInfo(filePath);
+                Decompress(file);
+            }
+        }
+
+        public static void Compress(FileInfo fileToCompress)
+        {
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".cmp"))
+                {
+                    using (DeflateStream compressionStream = new DeflateStream(compressedFileStream, CompressionMode.Compress))
+                    {
+                        originalFileStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+        }
+
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (DeflateStream decompressionStream = new DeflateStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine("Decompressed: {0}", fileToDecompress.Name);
+                    }
+                }
+            }
+        }
+    }
+}
